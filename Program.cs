ecf723e77b70c34deac737c6e4ff1131027a85e9
Пономарев Денис <desa-196@yandex.ru>
2024-03-11 15:22:27 +0300

    Доделал код из семинара

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Lesson_005_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICalculator calculator = new Calculator();
+
+            calculator.GetResult += Calculator_GetResult;
+
+            while (true)
+            {
+                Console.Write("Введите число: ");
+
+                if (!Double.TryParse(Console.ReadLine(), out double number))
+                {
+                    Console.WriteLine("Ошибка ввода числа!");
+                    continue;
+                }
+
+
+                while (true)
+                {
+
+                    Console.Write("Введите операцию: +, -, *, /, 'q' - для выхода из программы, 'с' - для возврата к предидущему значению: ");
+                    string operation = Console.ReadLine();
+
+                    switch (operation)
+                    {
+                        case "+":
+                            calculator.Sum(number);
+                            break;
+                        case "-":
+                            calculator.Substract(number);
+                            break;
+                        case "*":
+                            calculator.Multiply(number);
+                            break;
+                        case "/":
+                            calculator.Divide(number);
+                            break;
+                        case "c":
+                            calculator.CancelLast();
+                            break;
+                        case "q":
+                            return;
+                        default:
+                            Console.WriteLine("Неверная команда");
+                            continue;
+                    }
+                    break;
+                }
+
+
+            }
+
+        }
+        private static void Calculator_GetResult(object? sender, OperandChangedEventArgs e)
+        {
+            Console.WriteLine(e.Operand);
+        }
+
+    }
+}
